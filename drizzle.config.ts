33d8d4e481be2f63793d526ce890b3da8b77d4f8
JayDave23 <130729867+JayDave23@users.noreply.github.com>
2025-02-05 19:29:49 +0530

    Update

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,5 +3,5 @@ export default {
     out: "./drizzle", 
     dialect: "postgresql", 
     connectionString: process.env.DATABASE_URL,
-    url: "postgresql://neondb_owner:npg_7bGJk0ZlmHrL@ep-floral-sound-a8m3nrbk-pooler.eastus2.azure.neon.tech/neondb?sslmode=require"  ,
+    url:  process.env.DATABASE_URL  ,
   } 
